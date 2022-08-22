f86475603c79f8ed5269bf3cfd12240d1152222a
MatheusFontenele <matheus.fontenele@icloud.com>
2022-08-21 22:26:31 -0300

    feat: simple sidebar

--- components/SIdebar.tsx
+++ components/SIdebar.tsx
@@ -7,51 +7,52 @@ import {
 
 export function Sidebar() {
 	return (
+		//flex w-full h-screen fixed z-20 backdrop-blur-sm backdrop-brightness-150
 		<aside className="flex w-full h-screen fixed z-20 backdrop-blur-sm backdrop-brightness-150">
-			<div className="flex absolute gap-12 w-96 h-screen p-8 bg-black items-start flex-col z-40 ">
+			<div className="flex absolute gap-12 h-screen p-8 bg-black items-center flex-col z-40 ">
 				<div className="">
 					<span className="font-bold text-gray-400">GERAL</span>
-					<div className="flex gap-4 mt-6 items-stretch flex-col">
+					<div className="flex gap-4 mt-6 items-center flex-col">
 						<a href="" className="flex items-center gap-4 text-pink-400">
-							<RiDashboardLine />
-							<span className="font-medium">Dashboard</span>
+							<RiDashboardLine size={25} />
+							<span className="font-medium hidden">Dashboard</span>
 						</a>
 						<a href="" className="flex items-center gap-4 ">
-							<RiContactsLine />
-							<span className="font-medium">Usuarios</span>
+							<RiContactsLine size={25} />
+							<span className="font-medium hidden">Usuarios</span>
 						</a>
 						<a href="" className="flex items-center gap-4 text-pink-400">
-							<RiDashboardLine />
-							<span className="font-medium">Dashboard</span>
+							<RiDashboardLine size={25}/>
+							<span className="font-medium hidden">Dashboard</span>
 						</a>
 						<a href="" className="flex items-center gap-4 ">
-							<RiContactsLine />
-							<span className="font-medium">Usuarios</span>
+							<RiContactsLine size={25}/>
+							<span className="font-medium hidden">Usuarios</span>
 						</a>
 						<a href="" className="flex items-center gap-4 text-pink-400">
-							<RiDashboardLine />
-							<span className="font-medium">Dashboard</span>
+							<RiDashboardLine size={25}/>
+							<span className="font-medium hidden">Dashboard</span>
 						</a>
 					</div>
 				</div>
 				<div className="">
-					<span className="font-bold text-gray-400">AUTOMACAO</span>
-					<div className="flex gap-4 mt-6 items-stretch flex-col">
+					<span className="font-bold text-gray-400">AUTO</span>
+					<div className="flex gap-4 mt-6 items-center flex-col">
 						<a href="" className="flex items-center gap-4 ">
-							<RiInputMethodLine />
-							<span className="font-medium">Formularios</span>
+							<RiInputMethodLine size={25}/>
+							<span className="font-medium hidden">Formularios</span>
 						</a>
 						<a href="" className="flex items-center gap-4 ">
-							<RiGitMergeLine />
-							<span className="font-medium">Automacao</span>
+							<RiGitMergeLine size={25}/>
+							<span className="font-medium hidden">Automacao</span>
 						</a>
 						<a href="" className="flex items-center gap-4 ">
-							<RiGitMergeLine />
-							<span className="font-medium">Automacao</span>
+							<RiGitMergeLine size={25}/>
+							<span className="font-medium hidden">Auto</span>
 						</a>
 						<a href="" className="flex items-center gap-4 ">
-							<RiGitMergeLine />
-							<span className="font-medium">Automacao</span>
+							<RiGitMergeLine size={25}/>
+							<span className="font-medium hidden">Automacao</span>
 						</a>
 					</div>
 				</div>
